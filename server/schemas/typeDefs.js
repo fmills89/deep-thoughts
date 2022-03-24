56f47aad85064f9d82385155cb668188389def37
FoboIX <89666151+fmills89@users.noreply.github.com>
2022-03-24 17:23:04 -0500

    page navigation done

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,9 +1,7 @@
-// import the gql tagged template function
 const { gql } = require('apollo-server-express');
 
-// create our typeDefs
 const typeDefs = gql`
-type User {
+  type User {
     _id: ID
     username: String
     email: String
@@ -28,6 +26,11 @@ type User {
     username: String
   }
 
+  type Auth {
+    token: ID!
+    user: User
+  }
+
   type Query {
     me: User
     users: [User]
@@ -37,18 +40,12 @@ type User {
   }
 
   type Mutation {
-      login(email: String!, password: String!): Auth
-      addUser(username: String!, email: String!, password: String!): Auth
-      addThought(thoughtText: String!): Thought
-      addReaction(thoughtId: ID!, reactionBody: String!): Thought
-      addFriend(friendId: ID!): User
-  }
-
-  type Auth {
-      token: ID!
-      user: User
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addThought(thoughtText: String!): Thought
+    addReaction(thoughtId: ID!, reactionBody: String!): Thought
+    addFriend(friendId: ID!): User
   }
 `;
 
-// export the typeDefs
 module.exports = typeDefs;
